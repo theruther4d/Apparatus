bd850e433e4c2e322e8dec8ff0d2f1dbc7f7930d
Joshua Rutherford <joshua.rutherford1@gmail.com>
2016-03-17 08:26:19 -0700

    add tray menu, remove old menu, ✅ fixes #5

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 'use strict';
 
-// Dependencies:
+// Electron:
 const electron = require( 'electron' );
 const app = electron.app;
 const Tray = electron.Tray;
+const Menu = electron.Menu;
 const BrowserWindow = electron.BrowserWindow;
+const shell = electron.shell;
+
+// Dependencies:
 const exec = require( 'child_process' ).exec;
 const chokidar = require( 'chokidar' );
 const fs = require( 'fs' );
@@ -22,7 +26,7 @@ const runSequence = require( 'run-sequence' );
 const watch = require( 'gulp-watch' );
 
 // pre-delcare navIcon so it's not GC'd later:
-let navIcon;
+let navIcon, mainWindow;
 
 // Path constants:
 const BASE_DIR = app.getPath( 'userData' );
@@ -137,8 +141,6 @@ if( !fs.existsSync( npmDirectory) ) {
     });
 }
 
-var mainWindow = null;
-
 app.on( 'window-all-closed', () => {
     if( process.platform != 'darwin' ) {
         // quit gulp?
@@ -150,26 +152,61 @@ app.on( 'ready', () => {
     const electronScreen = electron.screen;
     const size = electronScreen.getPrimaryDisplay().workAreaSize;
 
+    // Menu:
+    var contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'Open Widgets Folder',
+            type: 'normal',
+            click: ( item, window ) => {
+                shell.showItemInFolder( WIDGET_DIR );
+            }
+        },
+        {
+            label: 'Show Debug Console',
+            type: 'checkbox',
+            checked: false,
+            click: ( item, window ) => {
+                const currWindow = mainWindow || window;
+                if( !currWindow ) {
+                    return;
+                }
+
+                const action = item.checked ? 'openDevTools' : 'closeDevTools';
+                currWindow.webContents[action]();
+            }
+        },
+        {
+            type: 'separator'
+        },
+        {
+            label: 'Quit Ubershit',
+            type: 'normal',
+            accelerator: 'Command+Q',
+            click: ( item, window ) => {
+                app.quit();
+            }
+        }
+    ]);
+
     // Create the notification bar icon:
     navIcon = new Tray( `${__dirname}/internal/images/dummy-icon.png` );
     navIcon.setToolTip( 'Ubershit' );
+    navIcon.setContextMenu( contextMenu );
 
     // Hide the dock icon:
     app.dock.hide();
 
     mainWindow = new BrowserWindow({
-        // type: 'desktop',
-        // transparent: true,
-        // frame: false,
-        // resizable: false,
-        // movable: false,
+        type: 'desktop',
+        transparent: true,
+        frame: false,
+        resizable: false,
+        movable: false,
         width: size.width,
         height: size.height
     });
 
     mainWindow.loadURL( `file://${OUTPUT_DIR}/index.html` );
-
-    mainWindow.webContents.openDevTools();
 
     const watcher = chokidar.watch( `${OUTPUT_DIR}`, {
         ignoreInitial: true,
